7f0eac32fca312e171eb812ac6ea4d575c3b562a
Dapplie <pipodany5@gmail.com>
2024-08-09 10:36:23 +0300

    Footer new

--- src/Pages/Home/Footer.svelte
+++ src/Pages/Home/Footer.svelte
@@ -4,7 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   </head>
 
 
@@ -17,7 +16,7 @@
     <div class="container bottom_border">
         <div class="row justify-content-center">
             <div class="col-auto text-center">
-                <h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+                <h5 class="headin5_amrc col_white_amrc pt2" Style="font-family: 'Impact'">Follow us</h5>
                 <ul class="footer_ul2_amrc list-unstyled">
                     <li>
                         <a href="https://www.instagram.com/by_mimz/?hl=en" class="d-inline-flex align-items-center">
@@ -44,9 +43,6 @@
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 
 </footer>
